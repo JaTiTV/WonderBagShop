411e057e04eff73a4941c8cbf6da246a0c316872
JaTiTV <info.jatitv@gmail.com>
2021-02-15 09:28:23 +0100

    Update TODO.java

--- src/main/java/de/jatitv/wonderbagshop/TODO.java
+++ src/main/java/de/jatitv/wonderbagshop/TODO.java
@@ -5,8 +5,12 @@ package de.jatitv.wonderbagshop;
 
 public class TODO {
 
-    //TODO Bei allen 3 WBS wird 1000 abgezogen
+    //ToDo Items bei kauf in das Inv
 
-    //TODO Items bei kauf in das Inv
+    //ToDo Multi Lang
+
+
+
+
 
 }

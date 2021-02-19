2e37160f6c51a6170e5668445d4d841f757ce61d
pascalgoedeke <pascal.goedeke@outlook.de>
2021-02-19 16:19:07 +0100

    update notify

--- src/main/java/de/jatitv/wonderbagshop/main/Main.java
+++ src/main/java/de/jatitv/wonderbagshop/main/Main.java
@@ -34,6 +34,7 @@ public final class Main extends JavaPlugin {
     public static Main plugin;
     public static Plugin a;
     public static Economy eco = null;
+    public static String update_version = null;
 
     @Override
     public void onEnable() {
@@ -68,6 +69,7 @@ public final class Main extends JavaPlugin {
         (new UpdateChecker(this, 89234)).getVersion((version) -> {
             String currentVersion = this.getDescription().getVersion();
             if (!currentVersion.equalsIgnoreCase(version)) {
+                update_version = version;
                 String foundVersion = ("§6A new version of §8[§2W§6B§9S§8]§6 was found!");
                 String yourVersion = ("§6Your version §c" + currentVersion + "§b.");
                 String latestVersion = ("§6Current version: §a" + version + "§b");
@@ -90,6 +92,25 @@ public final class Main extends JavaPlugin {
             }
 
         });
+        
+        /*
+        TODO: Später der Inhalt vom Join Listener:
+        
+        if(player.hasPermission("wasauchimmer") {
+            if(Main.update_version != null) {
+                String currentVersion = this.getDescription().getVersion();
+                String foundVersion = ("§6A new version of §8[§2W§6B§9S§8]§6 was found!");
+                String yourVersion = ("§6Your version §c" + currentVersion + "§b.");
+                String latestVersion = ("§6Current version: §a" + version + "§b");
+                String downloadVersion = ("§6You can download it here: §ehttps://www.spigotmc.org/resources/wonderbagshop.89234/");
+                player.sendMessage(DefultValue.PrefixHC + foundVersion);
+                player.sendMessage(DefultValue.PrefixHC + latestVersion);
+                player.sendMessage(DefultValue.PrefixHC + yourVersion);
+                player.sendMessage(DefultValue.PrefixHC + downloadVersion);
+            }
+        }
+        
+        */
 
     }
     public static Plugin thisp() {

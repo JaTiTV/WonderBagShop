bcc9b12aaa0390246c3563520bb878f23c49dc88
JaTiTV <info.jatitv@gmail.com>
2021-02-19 22:57:11 +0100

    Codeänderungen

--- src/main/java/de/jatitv/wonderbagshop/Listeners/JoinEvent.java
+++ src/main/java/de/jatitv/wonderbagshop/Listeners/JoinEvent.java
@@ -1,11 +1,20 @@
 // This claas was created by JaTiTV
 
+// ___________________________________________________________________________________
+//  __          __             _           ____               _____ _
+//  \ \        / /            | |         |  _ \             / ____| |
+//   \ \  /\  / /__  _ __   __| | ___ _ __| |_) | __ _  __ _| (___ | |__   ___  _ __
+//    \ \/  \/ / _ \| '_ \ / _` |/ _ \ '__|  _ < / _` |/ _` |\___ \| '_ \ / _ \| '_ \
+//     \  /\  / (_) | | | | (_| |  __/ |  | |_) | (_| | (_| |____) | | | | (_) | |_) |
+//      \/  \/ \___/|_| |_|\__,_|\___|_|  |____/ \__,_|\__, |_____/|_| |_|\___/| .__/
+//                                                      __/ |                  | |
+//                                                     |___/                   |_|
+// ___________________________________________________________________________________
 
 package de.jatitv.wonderbagshop.Listeners;
 
 import de.jatitv.wonderbagshop.DefultValue.DefultValue;
 import de.jatitv.wonderbagshop.main.Main;
-import de.jatitv.wonderbagshop.main.UpdateChecker;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.entity.Player;

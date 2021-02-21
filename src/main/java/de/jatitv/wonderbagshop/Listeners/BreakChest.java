56ea9592b0b0cd9375e34393c4b7f3d380b27194
JaTiTV <info.jatitv@gmail.com>
2021-02-21 06:41:23 +0100

    Update BreakChest.java

--- src/main/java/de/jatitv/wonderbagshop/Listeners/BreakChest.java
+++ src/main/java/de/jatitv/wonderbagshop/Listeners/BreakChest.java
@@ -3,21 +3,22 @@
 
 package de.jatitv.wonderbagshop.Listeners;
 
-import de.jatitv.wonderbagshop.DefultValue.DefultValue;
-import org.bukkit.Bukkit;
+import de.jatitv.wonderbagshop.DefultValue.DefultValue_WB1;
+import de.tr7zw.nbtapi.NBTBlock;
+import de.tr7zw.nbtapi.NBTItem;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class BreakChest implements Listener {
 
+    @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
         Block block = e.getBlock();
-        if (block.getType() == Material.CHEST) {
+        if (block.getType() == Material.CHEST){
             block.setType(Material.AIR);
-        } else {
-            Bukkit.getConsoleSender().sendMessage(DefultValue.PrefixHC + "§5ERROR");
         }
     }
 }
